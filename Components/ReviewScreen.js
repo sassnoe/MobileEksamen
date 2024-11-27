330cce4a2ce627a5962afa41855d4ef5eb6b1062
sassnoe <joakimsassnoe@gmail.com>
2024-11-27 13:31:41 +0100

    tabs

---
+++ Components/ReviewScreen.js
@@ -0,0 +1,12 @@
+// ./Components/ReviewScreen.js
+
+import React from "react";
+import { View, Text } from "react-native";
+
+export default function ReviewScreen() {
+  return (
+    <View>
+      <Text>Review Screen</Text>
+    </View>
+  );
+}
