b19702c9736251ea00df6643068fbc299d8b4755
Jaes98 <marcus.jaes@gmail.com>
2024-12-02 00:37:54 +0100

    added baseline map and start to save to firebase, needs more work

--- App.js
+++ App.js
@@ -1,10 +1,89 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import MapView, { Marker } from "react-native-maps";
+import { useState, useRef, useEffect } from "react";
+import * as Location from "expo-location";
 
 export default function App() {
+  const [markers, setMarkers] = useState([]);
+  const [region, setRegion] = useState({
+    // DK koordinater så det bliver vist først
+    latitude: 55,
+    longitude: 12,
+    latitudeDelta: 20,
+    longitudeDelta: 20,
+  });
+
+    const mapView = useRef(null);
+  const locationSubscribtion = useRef(null); //til når appen lukker og den ikke skal lytte mere
+
+  useEffect(() => {
+    async function startListening() {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status != "granted") {
+        alert("ingen adgang til lokation");
+        return;
+      }
+      locationSubscribtion.current = await Location.watchPositionAsync(
+        {
+          distanceInterval: 100,
+          accuracy: Location.Accuracy.High,
+        },
+        (lokation) => {
+          const newRegion = {
+            latitude: lokation.coords.latitude,
+            longitude: lokation.coords.longitude,
+            latitudeDelta: 20,
+            longitudeDelta: 20,
+          };
+          setRegion(newRegion); // flytter kortet til den nye lokation
+          if (mapView.current) {
+            mapView.current.animateToRegion(region);
+          }
+        }
+      );
+    }
+    startListening();
+    return () => {
+      if (locationSubscribtion.current) {
+        locationSubscribtion.current.remove();
+      }
+    };
+  }, []);
+
+    function addMarker(data) {
+    const { latitude, longitude } = data.nativeEvent.coordinate;
+    const newMarker = {
+      coordinate: { latitude, longitude },
+      key: data.timeStamp,
+      title: "Copenhagen",
+    };
+    setMarkers([...markers, newMarker]);
+    saveMarkerToFirestore(newMarker);
+  }
+
+
+  const saveMarkerToFirestore = async (marker) => {
+    try {
+      const db = firebase.firestore();
+      await db.collection("markers").add(marker);
+    } catch (error) {
+      console.error("Error adding marker: ", error);
+    }
+  };
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+     <View style={styles.container}>
+      <MapView style={styles.map} region={region} onLongPress={addMarker}>
+        {markers.map((marker) => (
+          <Marker
+            coordinate={marker.coordinate}
+            key={marker.key}
+            title={marker.title}
+          />
+        ))}
+      </MapView>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +95,9 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  map: {
+    width: "100%",
+    height: "100%",
   },
 });
