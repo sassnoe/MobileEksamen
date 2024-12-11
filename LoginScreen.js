b51b6111c1723454bb4d7e8f678230dc022be088
LauritsSchou <laurits.schou@gmail.com>
2024-12-11 15:04:14 +0100

    small changes

--- LoginScreen.js
+++ LoginScreen.js
@@ -1,4 +1,3 @@
-// LoginScreen.js
 import React, { useState } from "react";
 import { View, Text, TextInput, Button, Alert } from "react-native";
 import { signInWithEmailAndPassword } from "firebase/auth";
